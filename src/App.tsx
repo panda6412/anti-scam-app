41e0929c58d68249be8a41c570c7ba1853db2c18
francis_liou <francis_liou@trendmicro.com>
2025-02-18 18:45:23 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-// src/App.tsx
 import { useEffect, useState } from 'react'
-import { Routes, Route, useSearchParams } from 'react-router'
-import { jwtDecode } from 'jwt-decode'
 import './App.css'
 
 interface User {
@@ -10,50 +7,50 @@ interface User {
   iat: number
 }
 
-function Home() {
-  return <h1 className='text-2xl'>Home Page</h1>
-}
-
-function Dashboard() {
-  return <h1 className='text-2xl'>Dashboard</h1>
-}
-
 function App() {
-  const [searchParams] = useSearchParams()
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [user, setUser] = useState<User | null>(null)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  const [jwtToken, setJwtToken] = useState<string | null>(null)
 
   useEffect(() => {
-    const token = searchParams.get('jwtToken')
-    if (token) {
+    setJwtToken(localStorage.getItem('jwtToken'))
+  }, [])
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      console.log('xx', localStorage.getItem('jwtToken'))
       try {
-        const decoded = jwtDecode<User>(token)
-        console.log(decoded)
-        setUser(decoded)
+        const response = await fetch('http://localhost:3000/api/user', {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('jwtToken')}`, // or sessionStorage
+            'Content-Type': 'application/json'
+          }
+        })
+
+        if (!response.ok) {
+          throw new Error('Unauthorized')
+        }
+
+        const data = await response.json()
+        setUser(data.user)
         setIsAuthenticated(true)
       } catch (error) {
-        console.error('Invalid JWT Token', error)
+        console.error('Authentication failed', error)
       }
     }
-  }, [searchParams])
+
+    fetchUser()
+  }, [])
 
   if (!isAuthenticated) {
-    return <div className='flex h-screen items-center justify-center text-2xl text-red-500'>Unauthorized Access</div>
+    return <div className='flex h-screen items-center justify-center text-2xl text-red-500'>!Unauthorized Access {jwtToken || 'no token'}</div>
   }
 
   return (
     <div className='p-4'>
       <h1 className='text-xl'>Welcome, {user?.name}</h1>
-      <nav>
-        <a href='/' className='mr-4'>
-          Home
-        </a>
-        <a href='/dashboard'>Dashboard</a>
-      </nav>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-      </Routes>
     </div>
   )
 }
